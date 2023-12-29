760c8b55e765161d12bb1c53f77edfd9a2438246
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-12-29 18:20:55 +0100

    fix globals css

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -4,8 +4,10 @@ type Props = {
 
 export default function RootLayout({ children }: Props) {
   return (
-    <div className="h-full bg-slate-100">
-      <main>{children}</main>
+    <div className="h-full bg-slate-100 dark:bg-slate-800">
+      <main className="bg-slate-100 pb-20 pt-40 dark:bg-slate-800 ">
+        {children}
+      </main>
     </div>
   )
 }
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,14 @@
+import { Medal } from "lucide-react"
+
 export default function MarketingPage() {
-  return <div>marketing page</div>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-row gap-x-2">
+          <Medal className="h-6 w-6" />
+          <span>No 1 task management</span>
+        </div>
+      </div>
+    </div>
+  )
 }
