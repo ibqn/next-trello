2246ffdefc0368be6d64ff6260a4850c74ae40e9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-12-31 11:26:14 +0100

    move comps to marketing

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,4 @@
-import { Footer } from "@/components/footer"
-import { Navbar } from "@/components/navbar"
+import { Footer, Navbar } from "@/components/marketing"
 
 type Props = {
   children: React.ReactNode
