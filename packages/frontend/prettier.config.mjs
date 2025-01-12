a01c8e64cbe08f9a2170a13e5696eebc986f8c45
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-01-12 18:22:19 +0100

    change project structure

---
+++ packages/frontend/prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  endOfLine: "lf",
+  semi: false,
+  printWidth: 120,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+
+export default config
