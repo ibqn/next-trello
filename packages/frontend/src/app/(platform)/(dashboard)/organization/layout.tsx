546487507f44ea6cec7744cb6bb9fcc43863c6a8
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-01-19 13:40:44 +0100

    adjust padding for sidebar in organization layout

--- packages/frontend/src/app/(platform)/(dashboard)/organization/layout.tsx
+++ packages/frontend/src/app/(platform)/(dashboard)/organization/layout.tsx
@@ -9,7 +9,7 @@ export default function OrganizationLayout({ children }: Props) {
   return (
     <main className="mx-auto flex w-full max-w-6xl grow pt-20 md:pt-24 2xl:max-w-screen-xl">
       <div className="flex flex-1 gap-x-7">
-        <div className="hidden w-64 shrink-0 md:flex md:flex-col">
+        <div className="hidden w-64 shrink-0 pl-4 md:flex md:flex-col">
           <Sidebar />
         </div>
         {children}
